6fedb13027eb92bd8284687affdc23776506a66b
Brian Diaz <bidiaz101@gmail.com>
2021-09-22 13:13:48 -0500

    commit tree d19639b791862d1b99bd2fe20b8835edf76f7939

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorDivs = actors.map(actor => {
+    const movies = actor.movies.map(movie => <li key={movie}>{movie}</li>)
+
+    return (
+      <div key={actor.name}>
+        <h1>{actor.name}</h1>
+        {movies}
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorDivs}
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorDivs = directors.map(director => {
+    const movies = director.movies.map(movie => <li key={movie}>{movie}</li>)
+
+    return (
+      <div key={director.name}>
+        <h1>{director.name}</h1>
+        <ul>
+          {movies}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorDivs}
+    </div>
+  )
 }
 
 export default Directors;
